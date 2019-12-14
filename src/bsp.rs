eed1c025712033e063eabee52565bb291babe3df
vch9 <valentinchb@gmail.com>
2019-12-14 21:20:39 +0100

    Change bsp::structs visibility

--- src/bsp.rs
+++ src/bsp.rs
@@ -1,27 +1,23 @@
 pub enum Color{
-    BLACK
+    BLACK,
+    WHITE,
+    RED,
+    BLUE
 }
 
-struct Label {
-    coord : u32,
-    colored : bool
+pub struct Rect {
+    pub start : (u32, u32),
+    pub end : (u32, u32),
+    pub color : Option<Color>,
 }
 
-struct Rect {
-    couleur : Option<Color>,
-    xd : u32,
-    xf : u32,
-    yd : u32,
-    yf : u32
-}
-
-struct Line {
-    deb : (u32, u32),
-    fin : (u32, u32),
-    color : Option<Color>
+pub struct Line {
+    pub start : (u32, u32),
+    pub end : (u32, u32),
+    pub color : Option<Color>
 }
 
 enum Bsp {
     R (Option<Color>),
-    L (Label, Box<Bsp>, Box<Bsp>)
+    L (Line, Box<Bsp>, Box<Bsp>)
 }
