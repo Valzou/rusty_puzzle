930c8829cc8faa835b3aea6d6485c430345283f0
vch9 <valentinchb@gmail.com>
2019-12-16 17:23:21 +0100

    Main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 mod bsp;
+mod app;
+
+use std::env;
 
 fn main() {
-    println!("todo main");
+    let args: Vec<String> = env::args().collect();
+
+    let nodes: u32 = args[1].parse().unwrap();
+    let width: u32 = args[2].parse().unwrap();
+    let height: u32 = args[3].parse().unwrap();
+
+    let bsp = bsp::generate_bsp(nodes, width, height);
+    let lines = bsp::bsp_to_lines(&bsp);
+    let mut rects = bsp::bsp_to_rectangles(&bsp);
+
+    app::create_application(400, 400, &lines, &mut rects);
 }
